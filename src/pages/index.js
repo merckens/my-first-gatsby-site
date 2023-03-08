3ff39ec1c01f58a14190ce208e074d9a3bab0938
dary w. m. merckens <dary.merckens@gmail.com>
2023-03-07 17:35:12 -0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="a bunch of lunatics having lunch atop a skyscraper"
+        src="../images/lunch atop a skyscraper.jpg"
+      />
     </Layout>
   )
 }
